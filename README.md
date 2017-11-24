1671cce56c27895867b7db08016d1108e4e6df57
wangabeng <wangabeng@163.com>
2017-11-24 16:39:22 +0800

    添加静态方法

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ node使mongoose连接数据库
 node通过mongoose连接mongodb不用skema示例
 
 node通过mongoose连接mongodb用skema示例
+
+详细使用方法 参考：
+http://ourjs.com/detail/53ad24edb984bb4659000013
--- app.js
+++ app.js
@@ -25,26 +25,42 @@ kitty.save(() => {
 // 用skema示例
 // 1 引用mongoose模块
 var mongoose = require('mongoose');
+
 // 2 创建一个数据库连接
 var db = mongoose.createConnection('localhost','nasi');
+
 // 3 定义一个Schema
 var PersonSchema = new mongoose.Schema({
   name:String   //定义一个属性name，类型为String
 });
-// 4 创建一个model
+
+// 4-0 给PersonSchema创建静态方法
+PersonSchema.methods.find = () => {
+	// console.log('haha');
+	console.log(this);
+};
+
+// 4-1 创建一个model 同时创建一个集合 可以直接在model上操作数据库 比如增加一条数据 create， 等等。如果在schema上创建了静态方法 比如删除 就可以直接使用
 var PersonModel = db.model('Person',PersonSchema);
+
+
+
+// 5-0 创建一个personEntity实体
+var personEntity = new PersonModel({name:'Krouky444'});
 /*
-// 5 创建一个personEntity实体
-var personEntity = new PersonModel({name:'Krouky'});
 //打印这个实体的名字看看
 console.log(personEntity); //Krouky
 
 personEntity.save();
 */
-PersonModel.create({name:'Krouky22'}, (err) => {
+// 直接创建 用PersonModel的create方法
+/*PersonModel.create({name:'Krouky22'}, (err) => {
 	if (err) {
 		console.log(err);
 		return;
 	}
 	console.log('保存成功');
-})
+})*/
+
+// 5-1 直接在model上调用skema的静态方法
+personEntity.find();
